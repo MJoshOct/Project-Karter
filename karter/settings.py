ac68e5582c17103c9cfd6c7745779845b9744e8d
MkJosh <142152503+MJoshOct@users.noreply.github.com>
2025-08-31 15:23:06 +0000

    sell/listing is working

--- karter/settings.py
+++ karter/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "profile",
     "search",
     #"rent",
+    "sell",
 ]
 
 LOGIN_URL = "/accounts/login/"
--- sell/views.py
+++ sell/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
 
-# Create your views here.
+def sell(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("sell_success")
+    else:
+        form = ProductForm()
+    
+    return render(request, "sell/sell.html", {"form": form})
+
+def sell_success(request):
+    return render(request, "sell/sell_success.html")
